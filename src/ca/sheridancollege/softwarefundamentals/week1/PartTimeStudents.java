2b5907c55940395079936f115c158ba522f45006
kaur8826 <kaur8826@sheridancollege.ca>
2020-01-24 11:36:06 -0500

    new branch

---
+++ src/ca/sheridancollege/softwarefundamentals/week1/PartTimeStudents.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.softwarefundamentals.week1;
+
+/**
+ *
+ * @author 14167
+ */
+public class PartTimeStudents extends Student{
+    private int hoursOfStudy;
+    public PartTimeStudents(String givenName, String givenID)
+    {
+        super(givenName,givenID );
+    }
+    
+}
